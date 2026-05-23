2afb571b20dcf947b13a71a1ad0f1f68fc2b4f23
scminy0527-rgb <scminy0527@gmail.com>
2026-05-23 16:35:14 +0900

    *fix: 커뮤니티 메인 페이지 채팅방 리스트 컴포넌트 분리를 통하여 리빌드 최적화

--- src/pages/community/post/CommunityPostContainer.jsx
+++ src/pages/community/post/CommunityPostContainer.jsx
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ColumnBlock, ActionBtn, CategoryPill } from "../communityStyle";
 import T from "../communityTextStyle";
 import PostListSection from "./postComponents/PostListSection.jsx";
+import LiveChatListSection from "./postComponents/LiveChatListSection.jsx";
 import { useChatContext } from "../context/ChatContext";
 import { POST_CATEGORIES } from "../constants";
 import {
   HeaderBlock,
-  LiveChatRow,
   PostHeader,
   PostCategoryHeader,
   PostCategoryRow,
   AllChatButton,
 } from "./communityPostContainerStyle";
-import LiveChatCardCandidate1 from "../chat/chatComponents/chatCardCandidate/LiveChatCardCandidate1.jsx";
-import { getChatRooms } from "../communityApi/chatApi.js";
-import LiveChatCardCandidate1Skeleton from "../chat/skeleton/LiveChatCardCandidate1Skeleton.jsx";
 
 const S = {
   ColumnBlock,
   ActionBtn,
   CategoryPill,
   HeaderBlock,
-  LiveChatRow,
   PostHeader,
   PostCategoryHeader,
   PostCategoryRow,
@@ -31,28 +27,8 @@ const S = {
 
 // 컴포넌트
 const CommunityPostContainer = () => {
-  const { openChatRoom, openCreateChatRoom } = useChatContext();
+  const { openCreateChatRoom } = useChatContext();
   const [selectedTag, setSelectedTag] = useState("");
-
-  // 채팅방 관련
-  const [rooms, setRooms] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  // 최초 3개의 채팅방만 불러오기
-  useEffect(() => {
-    const load = async () => {
-      setIsLoading(true);
-      try {
-        const data = await getChatRooms(1, 3);
-        setRooms(data.rooms);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    load();
-  }, []);
 
   console.log("메인 영역 그려지기");
   return (
@@ -70,24 +46,7 @@ const CommunityPostContainer = () => {
           </S.ActionBtn>
         </S.HeaderBlock>
         {/* 채팅방 */}
-        {isLoading ? (
-          // <div>로딩중</div>
-          <S.LiveChatRow>
-            <LiveChatCardCandidate1Skeleton />
-            <LiveChatCardCandidate1Skeleton />
-            <LiveChatCardCandidate1Skeleton />
-          </S.LiveChatRow>
-        ) : (
-          <S.LiveChatRow>
-            {rooms.map(({ id, ...roomData }) => (
-              <LiveChatCardCandidate1
-                key={id}
-                {...roomData}
-                onJoin={() => openChatRoom(...roomData)}
-              />
-            ))}
-          </S.LiveChatRow>
-        )}
+        <LiveChatListSection />
         {/* 채팅방 모두 보기 버튼 */}
         <S.AllChatButton>
           <Link to={"/community/chat"}>전체 보기 →</Link>
